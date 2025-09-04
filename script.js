33d709350e7288b02944e3a2181520a642beb204
drapala23 <rafael.drapala@escola.pr.gov.br>
2025-09-04 10:58:25 -0300

    ok

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const avanca = document.querySelectorAll('.btn-proximo');
-const reiniciarBtn = ducument.geteElementbytd('btn.')
+const reiniciarBtn = ducument.geteElementbytd('btn-reiniciar
 
-avanca.forEach(Button=>
-Button.addEventListener
-)
+avanca.forEach(Button=>{
+Button.addEventListener('click,'function() {
+ const atual = ducument.querySelector(',ativo');
+ const proximoPasso= 'passo-'+this.getAttribute('data-proximo');
+
+ atual.classlist.remove('ativo');
+const proximoElemento= ducument.geteElementbytd(proximoPasso);
+    
+if(proximoElemento){
+   proximoElemento.classlist.add('ativo');
+}else {
+   console.error('Elemento com id "${proximoPasso}"não encontrado,')
+  }
+ });
+{);

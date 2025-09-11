dfd805b6ba600368a788f09cab114bf96d80469f
drapala23 <rafael.drapala@escola.pr.gov.br>
2025-09-11 10:57:55 -0300

    ok

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-const avanca = document.querySelectorAll('.btn-proximo');
-const reiniciarBtn = ducument.geteElementbytd('btn-reiniciar');
+const avanca = document.querySelectorAll('btn.proximo')
+const reiniciarBtn = document.getElementById('bth-reiniciar')
 
-avanca.forEach(Button=>{}
-button.addEventListener('click',function() {
- const atual = ducument.querySelector(',ativo');
- const proximoPasso= 'passo-'+this.getAttribute('data-proximo');
+avanca.forEach(button => {
+button.addEventListener('click', function () {
+const atual = document.querySelector('.ativo');
+const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
- atual.classlist.remove('ativo');
-const proximoElemento= ducument.geteElementbytd(proximoPasso);
-    
-if(proximoElemento){
-   proximoElemento.classlist.add('ativo');
-}else {
-   console.error('Elemento com id "${proximoPasso}"não encontrado.');
-  }
-  {);
+atual.classList.remove('ativo');
+const proximoElemento = document.getElementById(proximoPasso);
+
+if (proximoElemento) {
+proximoElemento.classList.add('ativo');
+} else {
+console.error(`Elemento com ID "${proximoPasso}"não encontrado.`);
+}
+});
+});
 
 
-// reiniciar o jogo ao clicar no botão de reiniciar
+// Reinicia o jogo ao clicar no botão de reinício
 if (reiniciarBtn) {
-    reiniciarBtn.addEventListener('click', () => {}
-      const atual = document.querySelectorAll('.ativo');
-      atual.classlist.remove('ativo');
-      document.getElementById('passo-0').classLista.add('ativo');
-
-    )}
+reiniciarBtn.addEventListener('click', () => {
+const atual = document.querySelector('.ativo');
+atual.classList.remove('ativo');
+document.getElementById('passo-0').classList.add('ativo');
+});
+}

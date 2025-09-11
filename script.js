360f18763aadb84aaf3635172a6fac8e5120b020
drapala23 <rafael.drapala@escola.pr.gov.br>
2025-09-11 09:55:20 -0300

    ok

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 const reiniciarBtn = ducument.geteElementbytd('btn-reiniciar
 
-avanca.forEach(Button=>{
+avanca.forEach(Button=>{}
 Button.addEventListener('click,'function() {
  const atual = ducument.querySelector(',ativo');
  const proximoPasso= 'passo-'+this.getAttribute('data-proximo');
@@ -12,7 +12,16 @@ const proximoElemento= ducument.geteElementbytd(proximoPasso);
 if(proximoElemento){
    proximoElemento.classlist.add('ativo');
 }else {
-   console.error('Elemento com id "${proximoPasso}"não encontrado,')
+   console.error('Elemento com id "${proximoPasso}"não encontrado.')
   }
- });
-{);
+  {);
+
+
+// reiniciar o jogo ao clicar no botão de reiniciar
+if (reiniciarBtn) {
+    reiniciarBtn.addEventListener('click', () => {}
+      const atual = document.querySelectorAll('.ativo');
+      atual.classlist.remove('ativo');
+      document.getElementById('passo-0').classLista.add('ativo');
+
+    )}
